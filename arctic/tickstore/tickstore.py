c4950c8f222e852c6a2ef0bd6801afcb326f8288
Phil Vars <pvars@competitioncontrols.com>
2021-11-08 15:31:19 -0500

    Read timezone fix

--- arctic/tickstore/tickstore.py
+++ arctic/tickstore/tickstore.py
@@ -356,7 +356,7 @@ class TickStore(object):
         mgr = _arrays_to_mgr(arrays, columns, index, columns, dtype=None, typ='array')
         rtn = pd.DataFrame(mgr)
         # Present data in the user's default TimeZone
-        rtn.index = rtn.index.tz_localize(mktz())
+        rtn.index = rtn.index.tz_localize(mktz('utc')).tz_convert(mktz())
 
         t = (dt.now() - perf_start).total_seconds()
         ticks = len(rtn)
